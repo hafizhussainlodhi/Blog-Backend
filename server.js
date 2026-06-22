245e83b2376b238ae49a4f262aa5947f8864639a
Hafiz M. Hussain <hasanlodhi000@gmail.com>
2026-06-22 23:18:43 +0500

    ddd

--- server.js
+++ server.js
@@ -714,7 +714,7 @@ app.use(express.json());
 
 // CORS configuration (Apne frontend localhost aur live URL dono ke liye)
 app.use(cors({
-    origin: ['https://blog-website-f3s6.vercel.app', 'https://blog-frontend-beige-three.vercel.app', 'https://blog-backend-7nnn.vercel.app'],
+    origin: ['blog-website-seven-sandy.vercel.app', 'https://blog-frontend-beige-three.vercel.app', 'https://blog-backend-7nnn.vercel.app'],
     methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
     credentials: true
 }));
